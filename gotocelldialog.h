8920a69758a5f03fdf7b468c7d01ea5c06697be4
Романов Вячеслав Олегович <VRomanov@gomel.rw>
2018-05-30 15:47:06 +0300

    Initial commit.

---
+++ gotocelldialog.h
@@ -0,0 +1,19 @@
+#ifndef GOTOCELLDIALOG_H
+#define GOTOCELLDIALOG_H
+
+#include "ui_gotocelldialog.h"
+#include <QDialog>
+
+class GoToCellDialog : public QDialog, public Ui::GoToCellDialog
+{
+    Q_OBJECT
+
+public:
+    GoToCellDialog(QWidget *parent = 0);
+    ~GoToCellDialog();
+
+private slots:
+    void on_lineEdit_textChanged();
+};
+
+#endif // GOTOCELLDIALOG_H
